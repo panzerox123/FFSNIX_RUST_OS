411468bf9b8d2de1586d51743bc15c5a5dea31a9
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-01-16 21:10:37 +0530

    Bootloader, Test VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@
 
 use core::panic::PanicInfo;
 
+static TEST: &[u8] = b"FFNIX_RUST";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in TEST.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x1F;
+        }
+    }
+    loop{}
 }
 
 #[panic_handler]
